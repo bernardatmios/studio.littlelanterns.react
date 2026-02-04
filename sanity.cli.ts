1019b592ac3609550e0f3e001b66446734b7e6b1
Bernard Willer <bernard@paytec.global>
2026-02-05 00:21:13 +0200

    Upgrade Sanity packages to v5.8.0 and configure `appId` for auto-updates.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -10,6 +10,7 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
+    appId: 'dvw7h9ykw4cgjmghnwrf5c3u',
     autoUpdates: true,
   }
 })
